451b433a97602bf689c6710079bb16d3cd0b64b5
Andy-Zi <andreas@zieglerit.org>
2024-05-31 15:31:25 +0200

    update keymap & taptimeout

--- keyboards/splitkb/aurora/corne/keymaps/Andy-Zi/config.h
+++ keyboards/splitkb/aurora/corne/keymaps/Andy-Zi/config.h
@@ -1,8 +1,4 @@
 #pragma once
 
-#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
-
-#define TAPPING_TOGGLE 1
-#define TAPPING_TERM 280
+#define TAPPING_TERM 150
 #define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE, UNICODE_MODE_MACOS, UNICODE_MODE_LINUX
