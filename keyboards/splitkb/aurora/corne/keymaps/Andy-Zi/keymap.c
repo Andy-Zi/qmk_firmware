1d588468d0fec910f3f146c9f5c7b37701b26467
Andy-Zi <andreas@zieglerit.org>
2024-08-12 22:19:33 +0200

    keymap update & flake

--- keyboards/splitkb/aurora/corne/keymaps/Andy-Zi/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/Andy-Zi/keymap.c
@@ -7,6 +7,8 @@ enum layer_names {
     _ADJUST,
 };
 
+// Layer 1 Modifier
+
 #define M_Z LSFT_T(KC_Z)
 #define M_X LCTL_T(KC_X)
 #define M_C LGUI_T(KC_C)
@@ -16,6 +18,16 @@ enum layer_names {
 #define M_COMM RGUI_T(KC_COMM)
 #define M_DOT RCTL_T(KC_DOT)
 #define M_SLSH RSFT_T(KC_SLSH)
+
+// Layer 2 Modifier
+//
+#define M_UNDS LCTL_T(KC_UNDS)
+#define M_MINS LGUI_T(KC_MINS)
+#define M_LBRC LALT_T(KC_LBRC)
+
+#define M_RBRC RALT_T(KC_RBRC)
+#define M_PLUS RGUI_T(KC_PLUS)
+#define M_EQL RCTL_T(KC_EQL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_split_3x6_3(
@@ -36,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, KC_UNDS, KC_MINS, KC_LBRC, KC_LCBR,                      KC_RCBR, KC_RBRC, KC_PLUS,  KC_EQL, _______, _______,
+      _______, KC_LSFT,  M_UNDS,  M_MINS,  M_LBRC, KC_LCBR,                      KC_RCBR,  M_RBRC,  M_PLUS,   M_EQL, KC_RSFT, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, KC_TRNS,KC_SPACE,   KC_ENTER,   MO(3), _______
                                       //`--------------------------'  `--------------------------'
@@ -45,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, _______, _______, _______, _______,  KC_DEL,                      KC_BSPC, _______, _______, _______, _______, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, KC_HOME, KC_PGUP, KC_PGDN,  KC_END,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,
+      KC_LALT, _______, KC_HOME, KC_PGUP, KC_PGDN,  KC_END,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_BTN1, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2, _______,
+      KC_LGUI, KC_BTN1, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______,   MO(3),KC_SPACE,   KC_ENTER, KC_TRNS, _______
                                       //`--------------------------'  `--------------------------'
@@ -59,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+      _______, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, _______,                      _______, KC_RALT, KC_RGUI, KC_RCTL, KC_RSFT, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, KC_TRNS,KC_SPACE,   KC_ENTER, KC_TRNS, _______
                                       //`--------------------------'  `--------------------------'
